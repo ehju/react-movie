bca502fb41f9e525ebde35eb69dd226a1e0e1ad3
Eunhye <junejc0613@naver.com>
2021-07-05 23:18:06 +0900

    Add about message

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,6 +1,14 @@
 import React from "react";
+import "./Home.css";
 
 function About(){
-    return(<section className="container">Hello</section>)
+    return(<section className="container">
+        <div className="content">
+            <h3>Enjoy Your Favorite Movie</h3>
+            
+            <p>Special thanks to <a href="https://nomadcoders.co/react-fundamentals">NomadCoder</a></p>
+        </div>
+            
+        </section>)
 }
 export default About;
